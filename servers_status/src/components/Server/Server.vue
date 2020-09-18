f4558780f44ee8ac6ee144f7a02d58afbbaaa27f
Chu-Te <ctchen002@gmail.com>
2020-09-18 17:02:56 +0800

    deploy

---
+++ servers_status/src/components/Server/Server.vue
@@ -0,0 +1,24 @@
+<template>
+    <li
+        class="list-group-item"
+        style="cursor:pointer"
+        @click="serverSelected">
+        Server <span style="color:blue">#{{ server.id }}</span>
+    </li>
+</template>
+
+<script>
+    import { serverBus } from '../../main'
+    export default {
+        props:['server'],
+        methods:{
+            serverSelected(){
+                serverBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
