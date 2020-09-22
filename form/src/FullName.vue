f1774f314d135d9fe2b3c554af9f73fdb77d1ff7
Chu-Te <ctchen002@gmail.com>
2020-09-22 14:02:46 +0800

    form

---
+++ form/src/FullName.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="">First Name</label>
+            <input type="text" class="form-control" :value="firstName" @input="nameChanged(true, $event)">
+        </div>
+        <div class="form-group">
+            <label for="">Last Name</label>
+            <input type="text" class="form-control" :value="lastName" @input="nameChanged(false, $event)">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['value'],
+    methods:{
+        nameChanged(isFirst, event){
+            let name = '';
+            if(isFirst){
+                name = event.target.value + ' ' + this.lastName; 
+            } else {
+                name = this.firstName + ' ' + event.target.value;
+            }//we need to know which part is revised because the full name is one variable.
+             //we have to remerge it
+            this.value = name;
+            this.$emit('input', this.value)
+        }
+    },
+    computed:{
+        firstName(){
+            return this.value.split(" ")[0];
+        },
+        lastName(){
+            return this.value.split(" ")[1];
+        }
+    }
+}
+</script>
+
+<style>
+</style>
