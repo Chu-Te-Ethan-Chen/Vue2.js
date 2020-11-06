2d6d6171e68d2559f3a4ee5de1bca671d932f092
Chu-Te <ctchen002@gmail.com>
2020-11-06 11:08:00 +0800

    delete

--- signup/src/axios-auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://identitytoolkit.googleapis.com/v1'
-})
-
-// instance.defaults.headers.common['SOMETHING'] = 'something'
-
-export default instance
